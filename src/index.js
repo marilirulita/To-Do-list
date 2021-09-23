ef0bf37df915ebb89ee72dbfae8d5f101a6a852a
marysol <marilirulita@gmail.com>
2021-09-23 13:12:08 -0700

    Add js functions show items

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import _ from 'lodash';
 import './style.css';
 
+let task = {
+  description: '',
+  completed: true,
+  index: 0
+}
+
+let tasksList = [1]; //array of objects
+
+function showItems() {
+  const listItems = getElementById('list-elem');
+  
+  for (let i = 0; i < tasksList.length; i++) {
+    listItems.innerHTML = "test";
+  }
+}
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   return element;
