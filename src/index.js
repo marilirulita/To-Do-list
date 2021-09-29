55c75815be27e95b204d9c818bff70f23b7b925b
marysol <marilirulita@gmail.com>
2021-09-28 17:31:22 -0700

    Add status module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import {status} from './status'; 
 
 const tasksList = [];
 let ids = 0;
@@ -14,6 +15,13 @@ class Task {
 tasksList.push(new Task('homework', false, (ids += 1)));
 tasksList.push(new Task('clean', false, (ids += 1)));
 tasksList.push(new Task('read', false, (ids += 1)));
+
+// module for change completed status
+tasksList.forEach(elem => {
+  if(elem.index == 1) {
+    console.log(elem.index);
+  }
+});
 
 const listItems = document.getElementById('list-elem');
 
---
+++ src/status.js
@@ -0,0 +1,7 @@
+// contain methods related to the status updates (completed: true / false).
+
+export function status(list) {
+  // logic: 
+  // take a list and look for the index anchange the completed options true/false
+ // list.forEach(elem => "you are here!" );
+}
